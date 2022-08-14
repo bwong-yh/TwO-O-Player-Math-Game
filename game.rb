f24ba5ab988b615c7660062161fefb0f6a26d8cc
Billy Wong <bwong.yh@protonmail.com>
2022-08-14 10:17:27 -0600

    create Game class

---
+++ game.rb
@@ -0,0 +1,12 @@
+class Game
+  def initialize
+    puts "New Game Started"
+    # create 2 players instance
+  end
+
+  # function for displaying score
+  
+  # function to check game over (either one player lives is 0)
+  
+  # function for swtiching players
+end
---
+++ main.rb
@@ -0,0 +1,4 @@
+require './game'
+
+# start game
+Game.new
